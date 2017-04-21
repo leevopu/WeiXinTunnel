d6dc646c8d007faf981540defcb15e990d0d14f3
leevopu <1311168315@qq.com>
2017-04-21 16:11:30 +0800

    ggg

--- src/test/User.java
+++ src/test/User.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class User {
 	private String id;
-	private String name;
+	private String xxx;
 	
 	public User(){
 	}
@@ -12,18 +12,12 @@ public class User {
 	public User(String id, String name, String address) {
 		super();
 		this.id = id;
-		this.name = name;
+		this.xxx = name;
 	}
 	public String getXXId() {
 		return id;
 	}
 	public void setDDId(String id) {
 		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
 	}
 }
