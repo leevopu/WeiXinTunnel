5c332bb6b85f8251e57f1d8d12b9514e3493c6ed
leevopu <1311168315@qq.com>
2017-04-21 10:23:26 +0800

    xxxx

---
+++ src/com/weixin/corp/entity/message/ImageMessage.java
@@ -0,0 +1,19 @@
+package com.weixin.corp.entity.message;
+
+
+public class ImageMessage extends BaseMessage {
+	/**
+	 * 只能在初始化时配置Image
+	 */
+	private Image Image;
+
+	public Image getImage() {
+		return Image;
+	}
+	
+	public ImageMessage(String mediaId){
+		this.Image = new Image(mediaId);
+	}
+
+}
+
