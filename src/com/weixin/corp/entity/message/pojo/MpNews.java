941b07c3c98ff15f540bf25302818cd14cf65591
leevopu <1311168315@qq.com>
2017-05-12 09:27:37 +0800

    ff

--- src/com/weixin/corp/entity/message/pojo/MpNews.java
+++ src/com/weixin/corp/entity/message/pojo/MpNews.java
@@ -1,20 +1,23 @@
 package com.weixin.corp.entity.message.pojo;
 
 public class MpNews {
-	/**
-	 * 图文消息名称
-	 */
 	private MpArticle[] articles;
-	
+
+	private String MediaId;
+
 	public MpArticle[] getArticles() {
 		return articles;
 	}
-	
 
-	public MpNews(MpArticle[] artricle) {
-		super();
-		//当前处理均为一条记录
-		articles=artricle;
+	public String getMediaId() {
+		return MediaId;
 	}
-	
+
+	public MpNews(String mediaId, MpArticle[] artricle) {
+		super();
+		// 当前处理均为一条记录
+		this.MediaId = mediaId;
+		this.articles = artricle;
+	}
+
 }
