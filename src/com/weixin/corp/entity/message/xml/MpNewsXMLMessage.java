8093041d47aa646a050e64ca5a47be5e5a20ad8f
itguan <3055582130@qq.com>
2017-05-16 10:58:15 +0800

    123466

--- src/com/weixin/corp/entity/message/xml/MpNewsXMLMessage.java
+++ src/com/weixin/corp/entity/message/xml/MpNewsXMLMessage.java
@@ -18,7 +18,7 @@ public class MpNewsXMLMessage extends CorpBaseXMLMessage {
 		return mpnews;
 	}
 
-	public MpNewsXMLMessage(String mediaId, String title,String thumb_media_id,String content,String digest) {
+	public MpNewsXMLMessage(String mediaId, String title,String thumb_media_id,String content) {
 		super();
 		MpArticle[] articles = {new MpArticle(title,thumb_media_id,content)};
 		this.mpnews = new MpNews("", articles);
