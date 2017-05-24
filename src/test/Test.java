69122c5ee55782e84a7c92c2816189028cfff2ec
leevopu <1311168315@qq.com>
2017-05-24 14:05:38 +0800

    xx

--- src/test/Test.java
+++ src/test/Test.java
@@ -14,14 +14,9 @@ public class Test {
 	    * @param args
 	    * 
 	    */
-	//{"department":[1,2],"email":"","enable":1,"gender":"","mobile":"","name":"abc","oaid":"","position":"","userid":"123","weixinid":""}
-	//{"department":[1,2],"email":"","enable":1,"gender":"","mobile":"","name":"abc","oaid":"","position":"","userid":"123","weixinid":""}
+	//{\"department\":[1,2],\"email\":\"\",\"enable\":1,\"gender\":\"\",\"mobile\":\"\",\"name\":\"abc\",\"oaid\":\"\",\"position\":\"\",\"userid\":\"123\",\"weixinid\":\"\"}
+	//{\"department\":[1,2],\"email\":\"\",\"enable\":1,\"gender\":\"\",\"mobile\":\"\",\"name\":\"abc\",\"oaid\":\"\",\"position\":\"\",\"userid\":\"123\",\"weixinid\":\"\"}
 	    public static void main(String[] args) {
-	    	User user = new User();
-	    	user.setUserid("123");
-	    	user.setName("abc");
-	    	user.getDepartment().add(1);
-	    	user.getDepartment().add(2);
-	    	System.out.println(JSONObject.fromObject(user).toString());
+	    	System.out.println(JSONObject.fromObject(null));;
 	    } 
 }
